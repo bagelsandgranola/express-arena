d49ecfe20d6a7f95930b11076af017046d45b562
Kyleigh <kysmith13@gmail.com>
2019-04-17 16:34:31 -0700

    google playstore server

--- app.js
+++ app.js
@@ -4,6 +4,54 @@ const app = express();
 
 app.use(morgan('dev'));
 
+app.get('/video', (req, res) => {
+    const video = {
+        title: 'Cats falling over',
+        description: '15 minutes of hilarious fun',
+        length: '15.40',
+    }
+    res.json(video)
+})
+
+app.get('/grade', (req, res) => {
+    const { mark } = req.query;
+
+    if (!mark) {
+        return res
+        .status(400)
+        .send("Please provide a mark")
+    }
+
+    const numericMark = parseFloat(mark);
+    if (Number.isNaN(numericMark)) {
+        return res
+        .status(400)
+        .send("Mark must be a numeric value");
+    }
+
+    if (numericMark < 0 || numericMark > 100) {
+        return res
+        .status(400)
+        .send("Mark must be in range of 0 to 100")
+
+    }
+
+    if (numericMark >= 90) {
+        return res
+        .send("A");
+    }
+    if (numericMark > 80) {
+        return res
+        .send("B");
+    }
+    if (numericMark >= 70) {
+        return res
+        .send("C");
+    }
+    res
+        .send("F");
+})
+
 app.get('/echo', (req, res) => {
     const responseText= `Here are some details of your request:
         Base URL: ${req.baseUrl}
---
+++ googlePlayStore/app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+app.use(morgan('common'));
+app.use(cors());
+
+const apps = require('./playstore');
+
+app.get('/apps', (req, res) =>
+{
+    const sort = req.query.sort;
+    const genres = req.query.genres;
+
+    if(sort) {
+        if(!['rating', 'app'].includes(sort)) {
+            return res
+            .status(400)
+            .send('Sort must be by rating or app')
+        }
+    }
+
+    if(genres) {
+        if(!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genres)) {
+            return res
+            .status(400)
+            .send('Genres must be Action, Puzzle, Strategy, Causal, Arcade or Card')
+        }
+    }
+
+    let results = apps;
+
+    if(genres)
+    {
+        results = apps.filter(app => 
+            app.genres.toLowerCase().includes(genres.toLowerCase())
+    );
+    }
+
+    if(sort)
+    {
+        results
+        .sort((a, b) => {
+          return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    })
+}
+    res
+        .json(results);
+
+});
+
+app.listen(6000, () => {
+    console.log('Server started on PORT 6000');
+})
